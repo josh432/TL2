6750cebfbfd0ea575eb052ea7252b06de2ffbf52
Josh Silverman <jrsilverman71@hotmail.com>
2019-05-23 14:03:02 -0700

    finished stoplight

--- trafficlight/src/components/TrafficLight.js
+++ trafficlight/src/components/TrafficLight.js
@@ -10,13 +10,37 @@ class TrafficLight extends React.Component {
 
   render() {
     console.log(this.state);
+
+    let redSelected = "";
+    if (this.state.theValue === "red") {
+      redSelected = "selected";
+    }
+
+    let yellowSelected = "";
+    if (this.state.theValue === "yellow") {
+      yellowSelected = "selected";
+    }
+
+    let greenSelected = "";
+    if (this.state.theValue === "green") {
+      greenSelected = "selected";
+    }
     return (
       <div>
         <div id="trafficLight" />
         <div id="container">
-          <div className="red light" />
-          <div className="yellow light" />
-          <div className="green light" />
+          <div
+            className={"red light " + redSelected}
+            onClick={() => this.setState({ theValue: "red" })}
+          />
+          <div
+            className={"yellow light " + yellowSelected}
+            onClick={() => this.setState({ theValue: "yellow" })}
+          />
+          <div
+            className={"green light " + greenSelected}
+            onClick={() => this.setState({ theValue: "green" })}
+          />
         </div>
       </div>
     );
